fd4fb029c3af25475caaad0ed54c3ec34c55c774
Daniel Orr <orrdaniel73@gmail.com>
2023-11-10 07:10:03 +0000

    Blueprint classes

--- src/main/kotlin/net/mcbrawls/blueprint/Blueprint.kt
+++ src/main/kotlin/net/mcbrawls/blueprint/Blueprint.kt
@@ -1,15 +1,93 @@
 package net.mcbrawls.blueprint
 
-import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.block.BlockState
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
 
-object Blueprint : ModInitializer {
-    const val MOD_ID = "blueprint"
-    const val MOD_NAME = "Blueprint"
+/**
+ * Represents a structure blueprint.
+ */
+data class Blueprint(
+    /**
+     * The block state palette.
+     */
+    val palette: List<BlockState>,
 
-    private val logger = LoggerFactory.getLogger(MOD_NAME)
+    /**
+     * The block states stored as their palette ids.
+     */
+    val palettedBlockStates: List<PalettedState>,
+) {
+    /**
+     * A list of the positions to their actual block states.
+     */
+    private val blockStates: Map<BlockPos, BlockState> = palettedBlockStates.associate { it.blockPos to palette[it.paletteIndex] }
 
-	override fun onInitialize() {
-		logger.info("Initializing $MOD_NAME")
-	}
+    /**
+     * Places this blueprint in the world at the given position.
+     */
+    fun place(world: ServerWorld, pos: BlockPos): Int {
+        blockStates.forEach { (offset, state) ->
+            world.setBlockState(pos.add(offset), state)
+        }
+
+        return blockStates.size
+    }
+
+    companion object {
+        /**
+         * The codec of this class.
+         */
+        val CODEC: Codec<Blueprint> = RecordCodecBuilder.create { instance ->
+            instance.group(
+                BlockState.CODEC.listOf()
+                    .fieldOf("palette")
+                    .forGetter(Blueprint::palette),
+                PalettedState.CODEC.listOf()
+                    .fieldOf("block_states")
+                    .forGetter(Blueprint::palettedBlockStates),
+            ).apply(instance, ::Blueprint)
+        }
+
+        /**
+         * Creates a blueprint from block states in the world.
+         */
+        fun create(world: World, corner1: BlockPos, corner2: BlockPos): Blueprint {
+            val palette: MutableList<BlockState> = mutableListOf()
+            val palettedBlockStates: MutableList<PalettedState> = mutableListOf()
+
+            val (min, max) = corner1.compared(corner2)
+            BlockPos.iterate(min, max).forEach { pos ->
+                // filter out air
+                val state = world.getBlockState(pos)
+                if (state.isAir) {
+                    return@forEach
+                }
+
+                // add to palette if not present already
+                if (!palette.contains(state)) {
+                    palette.add(state)
+                }
+
+                // add to paletted block states
+                val relativePos = pos.subtract(min)
+                val paletteId = palette.indexOf(state)
+                palettedBlockStates.add(PalettedState(relativePos, paletteId))
+            }
+
+            return Blueprint(palette, palettedBlockStates)
+        }
+
+        private fun BlockPos.compared(other: BlockPos): Pair<BlockPos, BlockPos> {
+            val box = Box(Vec3d.of(this), Vec3d.of(other))
+            val min = BlockPos.ofFloored(box.minX, box.minY, box.minZ)
+            val max = BlockPos.ofFloored(box.maxX, box.maxY, box.maxZ)
+            return min to max
+        }
+    }
 }
---
+++ src/main/kotlin/net/mcbrawls/blueprint/PalettedState.kt
@@ -0,0 +1,32 @@
+package net.mcbrawls.blueprint
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.util.math.BlockPos
+
+/**
+ * A paletted block state.
+ */
+data class PalettedState(
+    /**
+     * The offset position.
+     */
+    val blockPos: BlockPos,
+
+    /**
+     * The index of the block state in the palette.
+     */
+    val paletteIndex: Int
+) {
+    companion object {
+        /**
+         * The codec of this class.
+         */
+        val CODEC: Codec<PalettedState> = RecordCodecBuilder.create { instance ->
+            instance.group(
+                BlockPos.CODEC.fieldOf("offset").forGetter(PalettedState::blockPos),
+                Codec.INT.fieldOf("index").forGetter(PalettedState::paletteIndex)
+            ).apply(instance, ::PalettedState)
+        }
+    }
+}
