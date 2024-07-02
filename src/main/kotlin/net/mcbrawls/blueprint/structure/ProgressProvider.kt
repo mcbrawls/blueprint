c143a6d851717132e83337cefc51a2bb3500e6a5
Daniel Orr <orrdaniel73@gmail.com>
2024-07-02 12:14:26 +0100

    Complete constant progress provider

--- src/main/kotlin/net/mcbrawls/blueprint/structure/ProgressProvider.kt
+++ src/main/kotlin/net/mcbrawls/blueprint/structure/ProgressProvider.kt
@@ -2,4 +2,8 @@ package net.mcbrawls.blueprint.structure
 
 fun interface ProgressProvider {
     fun getProgress(): Float
+
+    companion object {
+        val COMPLETE: ProgressProvider = ProgressProvider { 1.0f }
+    }
 }
