7a3057a3ad8519c099f3e1e0027ec8f5c4600edb
Daniel Orr <orrdaniel73@gmail.com>
2025-01-12 19:54:42 +0000

    fix: deprecation

--- src/main/kotlin/net/mcbrawls/blueprint/block/entity/BlueprintBlockEntityTypes.kt
+++ src/main/kotlin/net/mcbrawls/blueprint/block/entity/BlueprintBlockEntityTypes.kt
@@ -6,20 +6,16 @@ import net.mcbrawls.blueprint.BlueprintMod
 import net.mcbrawls.blueprint.block.BlueprintBlocks
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.entity.BlockEntityType
-import net.minecraft.datafixer.TypeReferences
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
-import net.minecraft.util.Util
 
 object BlueprintBlockEntityTypes {
     val REGION_ID = register("region_id", FabricBlockEntityTypeBuilder.create(::RegionIdBlockEntity, BlueprintBlocks.POINT_REGION))
 
     private fun <T : BlockEntity> register(id: String, builder: FabricBlockEntityTypeBuilder<T>): BlockEntityType<T> {
-        val identifier = Identifier.of(BlueprintMod.MOD_ID, id)
-        val datafixType = Util.getChoiceType(TypeReferences.BLOCK_ENTITY, identifier.toString());
-        val type = builder.build(datafixType)
+        val type = builder.build()
         PolymerBlockUtils.registerBlockEntity(type)
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, identifier, type);
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(BlueprintMod.MOD_ID, id), type);
     }
 }
